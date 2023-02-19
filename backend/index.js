75c6f4234aa310565b0d2cb01aac532a350860dd
asfand687 <asfandyar687@gmail.com>
2023-02-20 00:52:41 +0500

    added connectdb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config()
 const express = require("express")
+const connectDB = require("./mongodb/connect")
 
 const app = express()
+connectDB(process.env.MONGODB_URI)
 const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
