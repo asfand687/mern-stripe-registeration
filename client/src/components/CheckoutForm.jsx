e33b14e01b9d2a9a1fe5bd36de191bf29d114eaf
asfand687 <asfandyar687@gmail.com>
2023-02-21 14:51:54 +0500

    added checkout form

---
+++ client/src/components/CheckoutForm.jsx
@@ -0,0 +1,101 @@
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import React, { useState } from 'react'
+
+const CheckoutForm = () => {
+  const [isPaymentLoading, setPaymentLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  const payMoney = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+    setPaymentLoading(true);
+    const response = await fetch("http://localhost:8080/get-client-secret", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: "Jared Leto",
+        email: "jared@gmail.com"
+      })
+    })
+
+    const data = await response.json()
+    console.log(data)
+    console.log(elements.getElement(CardElement))
+    const paymentResult = await stripe.confirmCardPayment(data.clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Jared Leto",
+          address: "114 St John Street, Montreal, Quebec"
+        },
+      },
+    })
+
+    setPaymentLoading(false)
+
+    if (paymentResult.error) {
+      alert(paymentResult.error.message);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Success!");
+      }
+    }
+
+  }
+
+
+  return (
+    <div
+      style={{
+        padding: "3rem",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "500px",
+          margin: "0 auto",
+        }}
+      >
+        <form
+          style={{
+            display: "block",
+            width: "100%",
+          }}
+          onSubmit={payMoney}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <CardElement
+              className="card"
+              options={{
+                style: {
+                  base: {
+                    backgroundColor: "white"
+                  }
+                },
+              }}
+            />
+            <button
+              type="submit"
+              className="pay-button"
+              disabled={isPaymentLoading}
+            >
+              {isPaymentLoading ? "Loading..." : "Pay"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutForm
