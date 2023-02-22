ef11c2ab7aea040b9b37f63191fbaa10f5ca0c10
asfand687 <asfandyar687@gmail.com>
2023-02-22 12:29:53 +0500

    added new endpoints

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const cors = require('cors')
 const connectDB = require("./mongodb/connect")
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+const User = require("./models/User")
 
 const app = express()
 connectDB(process.env.MONGODB_URI)
@@ -18,28 +19,40 @@ app.get("/", (req, res) => {
 
 // endpoint for creating a paymentIntent
 app.post('/get-client-secret', async (req, res) => {
-  const { username, email } = req.body;
+  const { username, email, paymentMethod } = req.body;
 
   try {
     // Create a PaymentIntent with the user's card details
+
+    const customer = await stripe.customers.create({
+      description: `Customer for MDHub- ${email}`,
+      email: email,
+      name: username,
+      payment_method: paymentMethod
+    })
+
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 1000, // Replace with the amount you want to charge in cents
-      currency: 'usd', // Replace with your preferred currency
-      automatic_payment_methods: { enabled: true },
+      amount: 10000, // Replace with the amount you want to charge in cents
+      currency: 'usd', // Replace with your preferred currency,
+      payment_method: paymentMethod,
+      customer: customer.id,
+      setup_future_usage: "on_session",
+      confirm: true,
       metadata: {
         username,
         email,
       },
-    });
+    },
+      (stripeErr, stripeRes) => {
+        if (stripeErr) {
+          res.status(500).json(stripeErr);
+        } else {
+          res.status(200).json(stripeRes);
+        }
+      });
 
 
-    // Extract payment method ID from the PaymentIntent
-    const paymentMethodId = paymentIntent.payment_method;
-
-    // Return client secret and payment method ID in response
-    res.status(200).json({
-      clientSecret: paymentIntent.client_secret,
-    })
+    console.log(paymentIntent)
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -61,6 +74,74 @@ app.post("/payment", (req, res) => {
       }
     }
   );
+})
+
+app.post("/create-customer", async (req, res) => {
+  await stripe.customers.create({
+    description: "Customer for MDHub",
+    email: req.body.email,
+    name: req.body.username
+  },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr)
+      } else {
+        res.status(200).json(stripeRes)
+      }
+    })
+})
+
+app.post("/get-card-digits", async (req, res) => {
+  try {
+    const paymentMethod = await stripe.paymentMethods.list({
+      customer: "cus_NOqLFVnxZMSKEA",
+      type: 'card'
+    })
+    res.status(200).json(paymentMethod)
+
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+app.post("/subscribe", async (req, res) => {
+  try {
+    const customerPaymentMethods = await stripe.paymentMethods.list({
+      customer: 'cus_NOqLFVnxZMSKEA',
+      type: 'card'
+    })
+
+    const paymentId = customerPaymentMethods.data[0].id
+
+    await stripe.paymentIntents.create({
+      amount: 30000, // Replace with the amount you want to charge in cents
+      currency: 'usd', // Replace with your preferred currency,
+      payment_method: paymentId,
+      customer: 'cus_NOqLFVnxZMSKEA',
+      setup_future_usage: "on_session",
+      confirm: true,
+      metadata: {
+        username: "freddymercury",
+        email: "freddy@gmail.com",
+      },
+    });
+
+    const subscription = await stripe.subscriptions.create({
+      customer: 'cus_NOqLFVnxZMSKEA',
+      items: [
+        {
+          price: 'price_1MdvMZE5iIaQvU2gCGMrAcEU',
+        },
+      ],
+      trial_period_days: 90,
+      default_payment_method: paymentId, // Replace this with the customer's payment method ID
+    })
+
+    res.status(200).json(subscription)
+
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 
